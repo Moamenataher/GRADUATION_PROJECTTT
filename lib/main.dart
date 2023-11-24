4fbc9789e1ced8284d212f84893e0852e212b526
Rawanfathy <90882551+Rawanfathy@users.noreply.github.com>
2023-11-24 17:02:47 +0200

    delete banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/widget/CustomNavBar.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    void main() {
+      runApp(const MyApp());
+    }
+
+    class MyApp extends StatelessWidget {
     return const  MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: CustomNavBar()
     );
   }
