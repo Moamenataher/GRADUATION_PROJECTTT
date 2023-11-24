38009f86a2029ac39cfb597b5ea1933192d529b5
Rawanfathy <90882551+Rawanfathy@users.noreply.github.com>
2023-11-24 17:13:24 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/widget/CustomNavBar.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    void main() {
-      runApp(const MyApp());
-    }
-
-    class MyApp extends StatelessWidget {
     return const  MaterialApp(
-      home: CustomNavBar()
+      debugShowCheckedModeBanner: false,
+        home: CustomNavBar()
     );
   }
 }
