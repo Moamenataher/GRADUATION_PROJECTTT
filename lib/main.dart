bd18ccce4b57274a652df41f7d57d38398ef11a7
Hossam Mohammad <>
2024-02-24 15:01:45 +0200

    - feat: added flutter_native_splash package to set app's native splash - feat: added flutter_screenutil to make app responsive - refactor: removed splash_page and replaced it with flutter_native_splash - style: removed unused package - refactor: glovy splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:graduation_project/pages/QrCode_page.dart';
-import 'package:graduation_project/pages/editprofile.dart';
-import 'package:graduation_project/pages/feedback&support_page.dart';
-import 'package:graduation_project/pages/home_Page.dart';
-import 'package:graduation_project/pages/instruction_page.dart';
-import 'package:graduation_project/pages/profile.dart';
-import 'package:graduation_project/pages/splashAndOnboarding_pages/onBoardingPages.dart';
-import 'package:graduation_project/pages/splashAndOnboarding_pages/splash_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graduation_project/widget/CustomNavBar.dart';
 
 void main() {
+
+   SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -20,10 +24,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return   const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CustomNavBar(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: CustomNavBar(),
+      ),
     );
   }
 }
-const Color primaryColor=Color(0xffABD6E5);
+
+const Color primaryColor = Color(0xffABD6E5);
